85b71d323ed4d8e743ac013a36a0f962885af81e
MonseUcan <113946623+MonseUcan@users.noreply.github.com>
2022-11-03 19:21:50 -0500

    se agrego la clase tripulación

--- Aerolinea/src/com/tuempresa/aerolinea/modelo/Tripulacion.java
+++ Aerolinea/src/com/tuempresa/aerolinea/modelo/Tripulacion.java
@@ -1,24 +1,23 @@
 package com.tuempresa.aerolinea.modelo;
 
-import java.util.*;
-
 import javax.persistence.*;
 
 import org.openxava.annotations.*;
 
 import lombok.*;
 
-@Entity @Getter @Setter
-
-public class Pais extends Identificable{
-	
+@Entity 
+@Getter @Setter
+public class Tripulacion extends Identificable{
 	
 	@Column(length=32)
-	@Required
+	@Required 
 	String nombre;
 	
-	@OneToMany(mappedBy="pais")
-	Collection<Tripulacion>tripulacion;
+	@ManyToOne(fetch=FetchType.LAZY,optional=true)
+	@DescriptionsList
+	Pais pais;
 	
 	
+
 }
